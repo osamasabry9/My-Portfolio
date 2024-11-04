25be3fc191ea7ab576dc9c27d1d148e523b37b4d
osamasabry9 <osamasabry324@gmail.com>
2024-11-04 23:21:27 +0400

    Add schema types in project - Add Projects schema

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+import skills from './skills'
+import projects from './projects'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [skills, projects ],
+}
